071fd0f8641f10cae5be546e396b4ce038c6075f
Parviz <perviznamazli021@gmail.com>
2022-11-12 17:05:44 +0400

    js file changed

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// let navElements = document.querySelectorAll("header nav .menu li a");
+
+// for (let i = 0; i < navElements.length; i++) {
+//   navElements[i].addEventListener("click", function () {
+//     if (navElements[i].classList.contains("active")) {
+//       this.classList.remove("active");
+//     } else {
+//       this.classList.add("active");
+//     }
+//   });
+// }
