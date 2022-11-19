7f6c2f467053f3030f6a5df4ecae4474c477483f
Parviz <perviznamazli021@gmail.com>
2022-11-19 12:12:59 +0400

    owlCarousel js file created

---
+++ js/owlCarousel.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 2,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    smartSpeed: 500,
+  });
+});
