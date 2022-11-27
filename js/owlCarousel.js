d342dc8d370f5852c1077aff68332e7bc863a51e
Parviz <perviznamazli021@gmail.com>
2022-11-27 20:40:01 +0400

    responsiveness added and issues fixed

--- js/owlCarousel.js
+++ js/owlCarousel.js
@@ -6,5 +6,17 @@ $(function () {
     autoplayTimeout: 3000,
     autoplayHoverPause: true,
     smartSpeed: 500,
+    responsiveClass: true,
+    //responsive carousel
+    responsive: {
+      //if width is more than 0px
+      0: {
+        items: 1,
+      },
+      //if width is more than 1200px
+      1200: {
+        items: 2,
+      },
+    },
   });
 });
